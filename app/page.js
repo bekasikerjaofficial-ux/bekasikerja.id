4a232e6490ff2cb20df51c61c99c4800f78fd472
bekasikerjaofficial-ux <bekasikerja.official@gmail.com>
2026-08-24 23:13:03 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>BekasiKerja.id</h1>
+      <p>Website siap digunakan.</p>
+    </div>
+  );
+}
