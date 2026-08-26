41c735762b7cc483c747dbcfd35c3a7a1817bf9f
bekasikerjaofficial-ux <bekasikerja.official@gmail.com>
2026-08-26 08:15:39 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,20 @@
+// app/layout.js
 import './globals.css';
+import { AppProvider } from './context/AppContext';
 
 export const metadata = {
-  title: 'BekasiKerja.id - Portal Lowongan Kerja, CV Builder & Psikotes Online',
-  description: 'Temukan lowongan kerja terbaru di Bekasi, buat CV standar ATS, dan ikuti simulasi psikotes online.',
+  title: 'BekasiKerja - Portal Loker & Karir Bekasi',
+  description: 'Portal Lowongan Kerja, Tes Skill, dan Lifestyle Karir',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        />
-      </head>
-      <body>{children}</body>
+      <body>
+        <AppProvider>
+          {children}
+        </AppProvider>
+      </body>
     </html>
   );
 }
